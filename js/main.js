1192972be4cd6ca785000538dbb785d796a344c8
ruixu <gengruixu@gmail.com>
2020-01-07 16:24:45 +0800

    change large
    change a template

--- js/main.js
+++ js/main.js
@@ -1,25 +1,113 @@
+jQuery(window).on('load', function() {
+	"use strict";
+    
+    
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");   
+    
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
 
-var time = 910800;
+        $("#intro .animation-container").each(function() {
 
-function timedCount()
-{
-document.getElementById('shijian').innerHTML = fmoney(time,0);
-time = time - 1;
-t = setTimeout("timedCount()",1000);
-}
+            var e = $(this);
 
-/** 每三位数字添加一个逗号，方便阅读 **/
+            setTimeout(function(){
 
-function fmoney(s, n)   
-{   
-   n = n > 0 && n <= 20 ? n : 2;   
-   s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";   
-   var l = s.split(".")[0].split("").reverse(),   
-   r = s.split(".")[1];   
-   t = "";   
-   for(i = 0; i < l.length; i ++ )   
-   {   
-      t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");   
-   }   
-   return t.split("").reverse().join("") /*+ "." + r*/;   
-}
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+    
+});
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+    
+    
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
+        
+    });
+    
+    
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated-from-right", { 
+        duration: 600,
+        delay: 0,
+        origin: "right",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+    
+    
+    // AJAX CONTACT FORM SUBMIT
+    $("#contact-form").submit(function(e) {
+
+        e.preventDefault();
+        var postdata = $(this).serialize();
+
+        $.ajax({
+
+            type: "POST",
+            url: "assets/php/contact.php",
+            data: postdata,
+            dataType: "json",
+            success: function(json) {
+
+                $("#contact-form input, #contact-form textarea").removeClass("error");
+
+                setTimeout(function(){
+
+                    if (json.nameMessage !== "") {
+
+                        $("#contact-form-name").addClass("error");
+
+                    }
+
+                    if (json.emailMessage !== "") {
+
+                        $("#contact-form-email").addClass("error");
+
+                    }
+
+                    if (json.messageMessage !== "") {
+
+                        $("#contact-form-message").addClass("error");
+
+                    }
+
+                }, 10);
+
+                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
+
+                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
+                    $('#contact-form').addClass("success");
+                    $('#contact-form textarea, #contact-form input').attr("placeholder","");
+                    $('#contact-form input, #contact-form button, #contact-form textarea').val('').prop('disabled', true);
+
+                }
+
+            }
+
+        });
+
+    });
+
+    
+});
